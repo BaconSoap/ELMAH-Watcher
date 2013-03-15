f7f23c3c2d566bff6309ca81328b53c80a2e8ea9
Andrew Varnerin <ffac06@gmail.com>
2013-03-15 10:14:27 -0400

    Added more options

--- options.js
+++ options.js
@@ -1,18 +1,58 @@
 var elmahWatcherOptions =
 (function($j){
+	
+	var previous;
 	function start(){
 		$j(init);
 	}
 
 	function init(){
 		chrome.storage.sync.get(['pageWatchPattern', 'pageRefreshInterval', 'codePattern', 'messageDisplay'], function(val){
-			setFromStorage("#watchPagePattern", val, "pageWatchPattern", "elmah.axd");
-			setFromStorage("#refreshInterval", val, "pageRefreshInterval", 0);
-			setFromStorage("#codePattern", val, "codePattern", "code|client code");
-			setFromStorage("#messageDisplay", val, "messageDisplay", "[code|client code] - [error|message]")
+			previous = val;
+			loadAllFromObject(val);
 		})
 
 		$j('#saveOptions').on('click', saveOptions);
+		$j("#getJson").on('click', getJson);
+		$j("#readJson").on('click', readJson);
+		$j("#resetToDefault").on('click', resetToDefault);
+		$j("#resetToPrevious").on('click', resetToPrevious);
+	}
+
+	function loadAllFromObject(obj){
+		setFromStorage("#watchPagePattern", obj, "pageWatchPattern", "elmah.axd");
+		setFromStorage("#refreshInterval", obj, "pageRefreshInterval", 0);
+		setFromStorage("#codePattern", obj, "codePattern", "code|client code");
+		setFromStorage("#messageDisplay", obj, "messageDisplay", "[code|client code] - [error|message]")
+	}
+
+	function resetToDefault(){
+		var obj = {};
+		loadAllFromObject(obj);
+	}
+
+	function resetToPrevious(){
+		loadAllFromObject(previous);
+	}
+
+	function getJson(){
+		var obj = {};
+		var matcher = $j('#watchPagePattern').val();
+		var interval = $j("#refreshInterval").val();
+		var codePattern = $j("#codePattern").val();
+		var messageDisplay = $j("#messageDisplay").val();
+
+		obj['pageWatchPattern'] = matcher;
+		obj['pageRefreshInterval'] = interval;
+		obj['codePattern'] = codePattern;
+		obj['messageDisplay'] = messageDisplay;
+
+		$j("#jsonArea").val(JSON.stringify(obj));
+	}
+
+	function readJson(){
+		var obj = JSON.parse($j("#jsonArea").val());
+		loadAllFromObject(obj);
 	}
 
 	function getDefaultVal(obj, prop, def){
